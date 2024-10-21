4e0fe41e9575e80fed7574b97d094add970e5c6f
ivanchen <ivan.chen@dlinkcorp.com>
2024-10-21 19:04:18 +0800

    Implement error messages

--- LoginApp/LoginApp/LoginView.swift
+++ LoginApp/LoginApp/LoginView.swift
@@ -7,23 +7,59 @@
 
 import SwiftUI
 
+struct LoginError {
+    var account: String = ""
+    var password: String = ""
+    
+    mutating func changeAccountMessage(message: String) {
+        self.account = message
+    }
+    
+    mutating func changePasswordMessage(message: String) {
+        self.password = message
+    }
+}
+
 struct LoginView: View {
     @State private var account: String = ""
     @State private var password: String = ""
+    @State private var accountIsDirty: Bool = false
+    @State private var passwordIsDirty: Bool = false
+    @State private var loginError = LoginError()
+    
+    func clearLoginError() {
+        loginError = LoginError()
+    }
     
     var isFormValid: Bool {
-        if (account.isEmpty || account.count < 8 || account.count > 64) {
-            return false
+        DispatchQueue.main.async {
+            clearLoginError()
         }
-        
-        if (!checkEmail(email: account)) {
+
+        if account.isEmpty || account.count < 8 || account.count > 64 {
+            DispatchQueue.main.async {
+                loginError.changeAccountMessage(message: "Invalid account")
+            }
+
             return false
         }
 
-        if (password.isEmpty || password.count < 8 || password.count > 64) {
+        if !checkEmail(email: account) {
+            DispatchQueue.main.async {
+                loginError.changeAccountMessage(message: "Invalid account")
+            }
+
             return false
         }
-        
+
+        if password.isEmpty || password.count < 8 || password.count > 64 {
+            DispatchQueue.main.async {
+                loginError.changePasswordMessage(message: "Invalid password")
+            }
+
+            return false
+        }
+
         return true
     }
     
@@ -31,7 +67,21 @@ struct LoginView: View {
         Form {
             TextField("Account", text: $account)
                 .textInputAutocapitalization(.never)
+                .onChange(of: account) { oldValue, newValue in
+                    accountIsDirty = true
+                }
+            if (accountIsDirty && !loginError.account.isEmpty) {
+                Text(loginError.account)
+                    .foregroundStyle(.red)
+            }
             SecureField("Password", text: $password)
+                .onChange(of: password) { oldValue, newValue in
+                    passwordIsDirty = true
+                }
+            if (passwordIsDirty && !loginError.password.isEmpty) {
+                Text(loginError.password)
+                    .foregroundStyle(.red)
+            }
             Button("Sign in") {
                 print("Your account is \(account)")
                 print("Your password is \(password)")
