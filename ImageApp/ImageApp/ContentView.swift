8b83202150f97e9033a827cf6caaf573f92792a4
ivanchen <ivan.chen@dlinkcorp.com>
2024-10-22 18:03:26 +0800

    Animation

--- ImageApp/ImageApp/ContentView.swift
+++ ImageApp/ImageApp/ContentView.swift
@@ -27,9 +27,13 @@ struct ContentView: View {
     private let imageURL: String = "https://img.uxcel.com/tags/ios-1721717446446-2x.jpg"
     
     var body: some View {
-        AsyncImage(url: URL(string: imageURL)) { phase in
+        AsyncImage(url: URL(string: imageURL), transaction: Transaction(animation: .spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0.25))) { phase in
             if let image = phase.image {
-                image.imageModifier()
+                image
+                    .imageModifier()
+                    //.transition(.move(edge: .bottom))
+                    //.transition(.slide)
+                    .transition(.scale)
             } else if phase.error != nil {
                 Image(systemName: "ant.circle.fill").iconModifier()
             } else {
