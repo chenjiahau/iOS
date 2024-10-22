574a1070d481d27fede2aae1e015c6d11dc168b2
ivanchen <ivan.chen@dlinkcorp.com>
2024-10-22 18:01:20 +0800

    Move modifiers into Image extension

--- ImageApp/ImageApp/ContentView.swift
+++ ImageApp/ImageApp/ContentView.swift
@@ -7,25 +7,33 @@
 
 import SwiftUI
 
+extension Image {
+  func imageModifier() -> some View {
+    self
+      .resizable()
+      .scaledToFit()
+  }
+  
+  func iconModifier() -> some View {
+    self
+      .imageModifier()
+      .frame(maxWidth: 128)
+      .foregroundColor(.purple)
+      .opacity(0.5)
+  }
+}
+
 struct ContentView: View {
     private let imageURL: String = "https://img.uxcel.com/tags/ios-1721717446446-2x.jpg"
     
     var body: some View {
         AsyncImage(url: URL(string: imageURL)) { phase in
             if let image = phase.image {
-                image
-                    .resizable()
-                    .scaledToFit()
+                image.imageModifier()
             } else if phase.error != nil {
-                Image(systemName: "ant.circle.fill")
-                    .frame(maxWidth: 128)
-                    .foregroundColor(.purple)
-                    .opacity(0.5)
+                Image(systemName: "ant.circle.fill").iconModifier()
             } else {
-                Image(systemName: "photo.circle.fill")
-                    .frame(maxWidth: 128)
-                    .foregroundColor(.purple)
-                    .opacity(0.5)
+                Image(systemName: "photo.circle.fill").iconModifier()
             }
         }
     }
