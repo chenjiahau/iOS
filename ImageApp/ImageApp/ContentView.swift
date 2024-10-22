c5fcb8c93a95961ed961e1b3f35a81382655e8ae
ivanchen <ivan.chen@dlinkcorp.com>
2024-10-22 17:59:38 +0800

    Phase

--- ImageApp/ImageApp/ContentView.swift
+++ ImageApp/ImageApp/ContentView.swift
@@ -11,17 +11,23 @@ struct ContentView: View {
     private let imageURL: String = "https://img.uxcel.com/tags/ios-1721717446446-2x.jpg"
     
     var body: some View {
-        AsyncImage(url: URL(string: imageURL)) { image in
-            image
-                .resizable()
-                .scaledToFit()
-        } placeholder: {
-            Image(systemName: "photo.circle.fill")
-                .frame(maxWidth: 128)
-                .foregroundColor(.purple)
-                .opacity(0.5)
+        AsyncImage(url: URL(string: imageURL)) { phase in
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .scaledToFit()
+            } else if phase.error != nil {
+                Image(systemName: "ant.circle.fill")
+                    .frame(maxWidth: 128)
+                    .foregroundColor(.purple)
+                    .opacity(0.5)
+            } else {
+                Image(systemName: "photo.circle.fill")
+                    .frame(maxWidth: 128)
+                    .foregroundColor(.purple)
+                    .opacity(0.5)
+            }
         }
-        .padding(40)
     }
 }
 
